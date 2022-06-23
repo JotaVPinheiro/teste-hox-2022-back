02c6d08103977c47ff858248674455d612748fec
JotaVPinheiro <jotavpinheiro@gmail.com>
2022-06-23 17:38:07 -0300

    added error routes

--- src/server.js
+++ src/server.js
@@ -5,6 +5,17 @@ const app = express();
 
 app.use(routes);
 app.use(express.json());
+
+app.use((req, res, next) => {
+  const error = new Error("Not Found!");
+  error.status = 404;
+  next(error);
+});
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ error: error.message });
+});
+
 app.listen(process.env.PORT || 3000, () =>
   console.log(`Server running on port ${process.env.PORT || 3000}`)
 );
