a10acb7077e9c20bfbca951b33fcc29f9096b94d
JotaVPinheiro <jotavpinheiro@gmail.com>
2022-06-24 18:26:55 -0300

    created products migration

---
+++ src/database/migrations/20220624205714_create_products_table.js
@@ -0,0 +1,13 @@
+exports.up = async (knex) =>
+  knex.schema.createTable("products", (table) => {
+    table.increments("id");
+    table.string("name").notNullable();
+    table.date("manufactureDate").notNullable();
+    table.boolean("perishable").notNullable();
+    table.date("expirationDate");
+    table.float("price").notNullable();
+
+    table.timestamps(0, 1, 1);
+  });
+
+exports.down = async (knex) => knex.schema.dropTable("products");
