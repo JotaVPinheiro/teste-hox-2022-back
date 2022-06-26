3f831f2fa884bee48d53753ce1e07db89268fc21
JotaVPinheiro <jotavpinheiro@gmail.com>
2022-06-26 20:05:34 -0300

    login route returns a jwt

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,5 +1,8 @@
 const knex = require("../database");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const expiresIn = "1h";
 
 const UserController = {
   async login(req, res, next) {
@@ -12,7 +15,9 @@ const UserController = {
       const rightPassword = await bcrypt.compare(password, user.password);
       if (!rightPassword) throw new Error("Wrong password.");
 
-      return res.json({});
+      const token = jwt.sign(user, "it_must_be_on_.env_but_ok", { expiresIn });
+
+      return res.json({ auth: true, token });
     } catch (error) {
       next(error);
     }
